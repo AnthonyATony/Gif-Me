6e588008daf14af6dc26e2edcbe0003c7387fe8a
cogofo <87773184+cogofo@users.noreply.github.com>
2021-07-27 10:22:17 -0500

    added additional categories

---
+++ Gif-Me !/MemesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemesViewController.swift
+//  Gif-Me !
+//
+//  Created by Charles Ogodo on 7/26/21.
+//
+
+import UIKit
+
+class MemesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

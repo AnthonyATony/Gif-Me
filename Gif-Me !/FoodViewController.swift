02da0f1329ea128efb85d047eb1ec643d218c3ab
cogofo <87773184+cogofo@users.noreply.github.com>
2021-07-28 11:06:44 -0500

    gave gifs to food

--- Gif-Me !/FoodViewController.swift
+++ Gif-Me !/FoodViewController.swift
@@ -13,12 +13,15 @@ class FoodViewController: UIViewController {
     var imageBre = ["bre1", "bre2", "bre3"]
     var imageDes = ["des1", "des2", "des3"]
     var imageGood = ["good1", "good2", "good3"]
+    
+    var gifFoods = ["bunch.gif", "carrot.gif", "stir.gif", "cake.gif"]
+    
     @IBAction func buttonImages(_ sender: UIButton) {
         mediaImage2.image = UIImage(named: "" )
         let num = Int.random(in: 0..<3)
         
         if num == 1 {
-        let r = Int(arc4random_uniform(UInt32(imageBre.count)))
+            let r = Int(arc4random_uniform(UInt32(imageBre.count)))
             mediaImage2.image = UIImage(named: imageBre[r] )
             
             
@@ -31,10 +34,10 @@ class FoodViewController: UIViewController {
             mediaImage2.image = UIImage(named: imageDes[r] )
             
             
-          
-                
-                
-            }
+            
+            
+            
+        }
         
         
         if num == 2 {
@@ -43,36 +46,33 @@ class FoodViewController: UIViewController {
             
             mediaImage2.image = UIImage(named: imageGood[r] )
             
-          
-       
             
-           
-            }
-                
-            }
-    override func viewDidLoad() {
+            
+            
+            
+        }
         
-        var imageDogs = ["dog1", "dog2", "dog3"]
-        var imageCats = ["cat 1", "cat 2", "cat 3"]
-        var imageBirds = ["bird1", "bird2", "bird3"]
-        var favoriteAnimals = [""]
-      var x = 0
-        
-        var gifFoods = ["bunch.gif", "carrot.gif", "stir.gif", "cake.gif"]
-        
-        
-        // Do any additional setup after loading the view.
     }
     
-//        @IBAction func onGifButton2Pressed(_ sender: UIButton) {
-//            let randomGif = gifFoods.randomElement()
-//            do {
-//                let gif = try UIImage(gifName: randomGif!)
-//                self.images.setGifImage(gif, loopCount: -1)
-//            } catch  {
-//                print(error)
-//        }
-//    }
-//
-//}
+    @IBAction func onGifButton2Pressed(_ sender: UIButton) {
+        let randomGif = gifFoods.randomElement()
+        do {
+            let gif = try UIImage(gifName: randomGif!)
+            self.mediaImage2.setGifImage(gif, loopCount: -1)
+        } catch  {
+            print(error)
+        }
+        
+    }
+    
 }
+
+func viewDidLoad() {
+    
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+
+
